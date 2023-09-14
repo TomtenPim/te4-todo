fb3167265b8a0dac11623e9de943df20792fe6a9
= <=>
2023-09-14 12:38:32 +0200

    ToFör  (init + ifattarbeta)

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import './App.css'
-import Todoitem from './components/Todoitem'
+import Todolist from './components/Todolist'
 
-function App() {
-  const todos = [
-    {id: 0, label: 'hammare', done: false},
-    {id: 1, label: 'hummer', done: true},
-  ]
-
-  const toggleTaskCompleted = (index) => {
-    console.log('toggleTaskCompleted', index)
-  }
-
-  return (
+function App(){
+  return(
     <div className="container">
       <h1>Vill du göra en sak?</h1>
-        <ul className='todo-list'>
-          {todos.map((todo, index) =>
-          <Todoitem
-            key={index}
-            id={ todo.id}
-            label={todo.label}
-            done={todo.done}
-            toggleTaskCompleted = {toggleTaskCompleted}
-            />
-          )}
-        </ul>
+      <Todolist />
     </div>
   )
 }
--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,15 +1,17 @@
-import style from "./todoitem.css"
+import './Todoitem.css'
 
 function Todoitem (props) {
     let {id, done,label} = props
 
     console.log(props)
     return (
-        <li>
+        <li className="todo-item">
             <label> {label} </label>
             <input 
                 checked={done}
-                onChange={ () => { props.toggleTaskCompleted(id)}}
+                onChange={ () => {
+                     props.toggleTaskCompleted(id)
+                    }}
                 type="checkbox"
             />
         </li>
